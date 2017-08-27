942f7878cd6955ed9ef01f2a220a49813fe55c6b
adam <amjacobson0@gmail.com>
2017-08-26 21:50:44 -0700

    Updated variables to ES6 standard

--- Column.js
+++ Column.js
@@ -1,96 +1,97 @@
 function Column() {
-	var ul;
-	
-	this.generate = function() {
-		var matrix = document.getElementById("matrix");
-		var height = document.documentElement.clientHeight;
+  let ul;
 
-		ul = document.createElement("ul");
-		matrix.appendChild(ul);
-		
-		do {
-			var li = document.createElement("li");
-			li.setAttribute("class", "invisible");
-			li.appendChild(document.createTextNode(randomChar()));
-			
-			ul.appendChild(li);
-			
-			if (typeof Column.liHeight == 'undefined') {
-				Column.liHeight = parseFloat(window.getComputedStyle(li).getPropertyValue('height'));
-				console.log("set liHeight", Column.liHeight);
-			}
-			
-			height -= Column.liHeight;
-		} while (height - Column.liHeight > 0)
-	}
-	
-	var randomChar = function() {
-		var halfWidthKana = "ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ";
-		var numerals = "0123456789";
-		
-		var possible = halfWidthKana + numerals;
+  this.generate = function() {
+    let matrix = document.getElementById("matrix");
+    let height = document.documentElement.clientHeight;
 
-		return possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	
-	var head = 0;
-	var chars = 7;
-	var tail = -chars;
-	
-	var animateStep = function() {
-		elements = ul.getElementsByTagName("li");
-		
-		if (elements[head]) {
-			toggleColor(elements[head]);
-		}
-		if (elements[head - 1]) {
-			toggleColor(elements[head - 1]);
-		}
-		if (elements[tail]) {
-			toggleColor(elements[tail]);
-		}
-		
-		head++;
-		tail++;
-		
-		if (head >= elements.length && tail >= elements.length) {
-			head = 0;
-			tail = -chars;
-		}
-	}
-	
-	this.animate = function() {
-		setInterval(function(){ animateStep(); }, 100);
-	}
-	
-	this.getWidth = function() {
-		var ulWidth = 0;
-		ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('width'));
-		ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('margin-left'));
-		ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('margin-right'));
-		
-		return ulWidth;
-	}
-	
-	var toggleColor = function(element) {
-		if (element.className == "invisible") {
-			element.className = "appear";
-		} else if (element.className == "appear") {
-			element.className = "visible";
-		} else if (element.className == "visible") {
-			element.className = "invisible";
-		} else {
-			element.className = "invisible";
-		}
-	}
-	
-	return {
-		animate: this.animate,
-		generate: this.generate,
-		getWidth: this.getWidth
-	};
+    ul = document.createElement("ul");
+    matrix.appendChild(ul);
+
+    do {
+      let li = document.createElement("li");
+      li.setAttribute("class", "invisible");
+      li.appendChild(document.createTextNode(randomChar()));
+
+      ul.appendChild(li);
+
+      if (typeof Column.liHeight === 'undefined') {
+        Column.liHeight = parseFloat(window.getComputedStyle(li).getPropertyValue('height'));
+        console.log("set liHeight", Column.liHeight);
+      }
+
+      height -= Column.liHeight;
+    } while (height - Column.liHeight > 0);
+  };
+
+  let randomChar = function() {
+    const halfWidthKana = "ｦｧｨｩｪｫｬｭｮｯｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ";
+    const numerals = "0123456789";
+
+    let possible = halfWidthKana + numerals;
+
+    return possible.charAt(Math.floor(Math.random() * possible.length));
+  };
+
+  let head = 0;
+  let chars = 7;
+  let tail = -chars;
+
+  let animateStep = function() {
+    let elements = ul.getElementsByTagName("li");
+
+    if (elements[head]) {
+      toggleColor(elements[head]);
+    }
+    if (elements[head - 1]) {
+      toggleColor(elements[head - 1]);
+    }
+    if (elements[tail]) {
+      toggleColor(elements[tail]);
+    }
+
+    head++;
+    tail++;
+
+    if (head >= elements.length && tail >= elements.length) {
+      head = 0;
+      tail = -chars;
+    }
+  };
+
+  this.animate = function() {
+    setInterval(function(){ animateStep(); }, 100);
+  };
+
+  this.getWidth = function() {
+    let ulWidth = 0;
+
+    ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('width'));
+    ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('margin-left'));
+    ulWidth += parseFloat(window.getComputedStyle(ul).getPropertyValue('margin-right'));
+
+    return ulWidth;
+  };
+
+  let toggleColor = function(element) {
+    if (element.className === "invisible") {
+      element.className = "appear";
+    } else if (element.className === "appear") {
+      element.className = "visible";
+    } else if (element.className === "visible") {
+      element.className = "invisible";
+    } else {
+      element.className = "invisible";
+    }
+  };
+
+  return {
+    animate: this.animate,
+    generate: this.generate,
+    getWidth: this.getWidth
+  };
 }
 
 Column.reset = function() {
-	Column.liHeight = undefined;
-}
+  Column.liHeight = undefined;
+};
