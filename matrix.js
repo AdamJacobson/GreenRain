2c0203f733749c5b9b32d9ef67979733bd518550
adam <amjacobson0@gmail.com>
2017-09-03 17:11:33 -0700

    Code cleanup

--- matrix.js
+++ matrix.js
@@ -1,58 +1,58 @@
 // Generate the Columns for the matrix
-generateColumns = function() {
-	var width = document.documentElement.clientWidth;
-	var columns = [];
-	
-	c = 0;
-	do {
-		columns[c] = new Column();
-		columns[c].generate();
-		
-		width -= columns[c].getWidth();
-		
-		c++;
-	} while (width - columns[c - 1].getWidth() > 0)
-	
-	return columns;
-}
+let generateColumns = function() {
+  var width = document.documentElement.clientWidth;
+  var columns = [];
+
+  let c = 0;
+  do {
+    columns[c] = new Column();
+    columns[c].generate();
+
+    width -= columns[c].getWidth();
+
+    c++;
+  } while (width - columns[c - 1].getWidth() > 0);
+
+  return columns;
+};
 
 // Animate the columns using random lengths and times
-animateColumns = function(columns) {
-	var maxWait = 15000;
-	
-	function setAnimateTimout(i) {
-		var wait = Math.floor((Math.random() * maxWait));
-		
-		setTimeout(function(){
-			columns[i].animate();
-		}, wait);
-	}
-	
-	for (i = 0; i < columns.length; i++) {
-		setAnimateTimout(i);
-	}
-}
+let animateColumns = function(columns) {
+  var maxWait = 15000;
 
-clearMatrix = function() {
-	var parent = document.getElementById("matrix");
-	var uls = document.getElementsByTagName("ul");
-	while (uls.length > 0) {
-		parent.removeChild(uls[0]);
-	}
-}
+  function setAnimateTimout(i) {
+    var wait = Math.floor((Math.random() * maxWait));
 
-generateMatrixText = function() {
-	clearMatrix();
-	Column.reset();
-	
-	var cols = generateColumns();
-	animateColumns(cols);
-}
+    setTimeout(function(){
+      columns[i].animate();
+    }, wait);
+  }
+
+  for (let i = 0; i < columns.length; i++) {
+    setAnimateTimout(i);
+  }
+};
+
+let clearMatrix = function() {
+  var parent = document.getElementById("matrix");
+  var uls = document.getElementsByTagName("ul");
+  while (uls.length > 0) {
+    parent.removeChild(uls[0]);
+  }
+};
+
+let generateMatrixText = function() {
+  clearMatrix();
+  Column.reset();
+
+  var cols = generateColumns();
+  animateColumns(cols);
+};
 
 window.onresize = function() {
-	window.onload();
-}
+  window.onload();
+};
 
 window.onload = function() {
-	generateMatrixText();
-}
+  generateMatrixText();
+};
